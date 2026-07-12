2dc49706897ccd6095c53ea8e75bbf9e3cfa58e5
ne05tane <kizah9419@proton.me>
2026-07-13 00:25:24 +0530

    define global variables to control threading

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from tkinter import *
+import threading
+import logic
+
+block_thread = None
+stop_event = threading.Event()
+is_blocking = False 
+
+
+
+
+
+
+
+
+
+
+
+
 
 window = Tk() 
 window.geometry("720x400")
 window.title("Website Blocker")
 window.config(background="#ffd6e5")
 
-window.mainloop() #to see preview
+window.mainloop() 
+
