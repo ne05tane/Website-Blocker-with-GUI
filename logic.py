9562d5e4cb085bf0aab4436bcf85fd93d9280fc7
ne05tane <kizah9419@proton.me>
2026-07-12 23:18:35 +0530

    define datetime

--- logic.py
+++ logic.py
@@ -4,5 +4,8 @@ import time
 path_to_host = "C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 
+#blocking logic
+
+end_time = datetime.datetime.now() + datetime.timedelta(hours=duration_hours)
 
 
